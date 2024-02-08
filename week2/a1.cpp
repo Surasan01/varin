4f81307b315b7d0c62030ae42a09d4492cc90a97
Surasan01 <141292276+Surasan01@users.noreply.github.com>
2024-02-08 14:46:51 +0700

    Update a1.cpp

--- week2/a1.cpp
+++ week2/a1.cpp
@@ -5,90 +5,90 @@ using namespace std;
 
 
 class Node {
-public:
-    string name;
-    int score;
-    Node *next;
-    Node *prev;
+    public:
+        string name;
+        int score;
+        Node *next;
+        Node *prev;
 
-    Node(string name, int score) {
-        this->name = name;
-        this->score = score;
-        next = nullptr;
-        prev = nullptr;
-    }
+        Node(string name, int score) {
+            this->name = name;
+            this->score = score;
+            next = nullptr;
+            prev = nullptr;
+        }
 };
 
 
 class Scoreboard {
-private:
-    Node *head;
-    Node *tail;
-    int size;   
-    int max;
+    private:
+        Node *head;
+        Node *tail;
+        int size;   
+        int max;
 
-public:
-    Scoreboard(int maxsize) {
-        head = nullptr;
-        tail = nullptr;
-        size = 0;
-        this->max = maxsize;
-    }
-    
-    
-    void insert(string name , int score){
-        Node *newNode = new Node(name,score);
-        if (head == nullptr){
-            head = newNode;
-            tail = newNode;
+    public:
+        Scoreboard(int maxsize) {
+            head = nullptr;
+            tail = nullptr;
+            size = 0;
+            this->max = maxsize;
         }
-        else {
-            Node *current = head ;
-            while (current != nullptr && score < current->score){
-                current = current->next;
-            }
-            if (current == nullptr){
-                tail->next = newNode;
-                newNode->prev = tail;
+    
+    
+        void insert(string name , int score){
+            Node *newNode = new Node(name,score);
+            if (head == nullptr){
+                head = newNode;
                 tail = newNode;
             }
-            else if (current == head){
-                head->prev = newNode;
-                newNode->next = head;
-                head = newNode;
-                // cout << head->score << endl;
-            }
             else {
-                // cout << current->score << endl;
-                newNode->next = current;
-                newNode->prev = current->prev;
-                current->prev->next = newNode;
-                current->prev = newNode;
+                Node *current = head ;
+                while (current != nullptr && score < current->score){
+                    current = current->next;
+                }
+                if (current == nullptr){
+                    tail->next = newNode;
+                    newNode->prev = tail;
+                    tail = newNode;
+                }
+                else if (current == head){
+                    head->prev = newNode;
+                    newNode->next = head;
+                    head = newNode;
+                    // cout << head->score << endl;
+                }
+                else {
+                    // cout << current->score << endl;
+                    newNode->next = current;
+                    newNode->prev = current->prev;
+                    current->prev->next = newNode;
+                    current->prev = newNode;
+                }
+            }
+            size++;
+            if (size > max){
+                remove();
             }
         }
-        size++;
-        if (size > max){
-            remove();
+    
+    
+        void remove(){
+            cout << tail->name << " was remove from the scoreboard" << endl;
+            tail = tail->prev;
+            tail->next = nullptr;
+            size--;
         }
-    }
-    
-    
-    void remove(){
-        cout << tail->name << " was remove from the scoreboard" << endl;
-        tail = tail->prev;
-        tail->next = nullptr;
-        size--;
-    }
     
 
     
-    void display(){
-        Node *current = head;
-        while (current != nullptr){
-            cout << current->name << " : " << current->score << endl;
-            current = current->next;
+        void display(){
+            Node *current = head;
+            while (current != nullptr){
+                cout << current->name << " : " << current->score << endl;
+                current = current->next;
+            }
         }
-    }
 };
 
 int main(){
