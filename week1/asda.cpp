f8e6d4163c6acd8dcc1ef7d56a7066b32de4ae6a
Surasan01 <141292276+Surasan01@users.noreply.github.com>
2024-02-23 14:59:29 +0700

    up

---
+++ week1/asda.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+string mytype="";
+
+void typembti3(){
+    mytype += "sursaklfj";
+    cout << mytype ;
+
+}
+
+int main() {
+    typembti3();
+    return 0;
+}
--- week1/mbti.cpp
+++ week1/mbti.cpp
@@ -14,6 +14,7 @@ class Data {
 
 class mbti{
     public:
+        string mytype="";
         Data data[50];
         float a[50];
         mbti(){
@@ -57,97 +58,107 @@ class mbti{
             a[i] = pow(dpq,0.5);
         }
     }
+
+    void mark_set(int &k){
+        int j;
+        float min=99;
+        for (j=0;j<15;j++){
+            if (a[j] < min){
+                min = a[j];
+                k=j;
+            }
+        }
+        // cout << k << endl;
+        a[k] = 100;
+    }
+
     void mark3(){
-        int i ,j,point[3];
-        float min=99;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[0]=j;
-            }
-        }
-        min=99;
-        a[point[0]] = 100;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[1]=j;
-            }
-        }
-        min=99;
-        a[point[1]] = 100;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[2]=j;
-            }
-        }
-        a[point[2]] = 100;
-        // for (i=0;i<3 ; i++){
-        //     cout << point[i] << endl;
-        // }
+        int point[3];
+        mark_set(point[0]);
+        mark_set(point[1]);
+        mark_set(point[2]);
+
+        column3(data[point[0]].type[0],data[point[1]].type[0],data[point[2]].type[0]);
+        column3(data[point[0]].type[1],data[point[1]].type[1],data[point[2]].type[1]);
+        column3(data[point[0]].type[2],data[point[1]].type[2],data[point[2]].type[2]);
+        column3(data[point[0]].type[3],data[point[1]].type[3],data[point[2]].type[3]);
     }
+
     void mark5(){
-        int i ,j,point[3];
-        float min=99;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[0]=j;
-            }
-        }
-        min=99;
-        a[point[0]] = 100;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[1]=j;
-            }
-        }
-        min=99;
-        a[point[1]] = 100;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[2]=j;
-            }
-        }
-        min=99;
-        a[point[2]] = 100;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[3]=j;
-            }
-        }
-        min=99;
-        a[point[3]] = 100;
-        for (j=0;j<15;j++){
-            if (a[j] < min){
-                min = a[j];
-                point[4]=j;
-            }
-        }
-        a[point[4]] = 100;
-        // for (i=0;i<5 ; i++){
-        //     cout << point[i] << endl;
-        // }
+        int point[5];
+        mark_set(point[0]);
+        mark_set(point[1]);
+        mark_set(point[2]);
+        mark_set(point[3]);
+        mark_set(point[4]);
+
+        column5(data[point[0]].type[0],data[point[1]].type[0],data[point[2]].type[0],data[point[3]].type[0],data[point[4]].type[0]);
+        column5(data[point[0]].type[1],data[point[1]].type[1],data[point[2]].type[1],data[point[3]].type[1],data[point[4]].type[1]);
+        column5(data[point[0]].type[2],data[point[1]].type[2],data[point[2]].type[2],data[point[3]].type[2],data[point[4]].type[2]);
+        column5(data[point[0]].type[3],data[point[1]].type[3],data[point[2]].type[3],data[point[3]].type[3],data[point[4]].type[3]);
 
     }
 
-    void typembti3(){
-        int i,j;
-        for (i=0;i<3;i++){
-            for (j=0;j<4;j++){
-                
+    void column3(char r1,char r2,char r3){
+        int i,a1=0,a2=0;
+        char r[3] = {r1,r2,r3};
+        
+        for (i=1;i<3;i++){
+            if (r[0]==r[i]){
+                a1++;
+                r[i]='#';
             }
         }
+        for (i=2;i<3 && r[1] != '#';i++){
+            if (r[1]==r[i]){
+                a2++;
+            }
+        }
+        if (a1>a2){
+            mytype += r1;
+        }
+        else {
+            mytype += r2;
+        }
     }
-    void typembti5(){
 
+    void column5(char r1,char r2,char r3,char r4,char r5){
+        int i,a1=0,a2=0,a3=0,a4=0;
+        char r[5] = {r1,r2,r3,r4,r5};
+        for (i=1;i<5;i++){
+            if (r[0]==r[i]){
+                a1++;
+                r[i]='#';
+            }
+        }
+        for (i=2;i<5 && r[1] != '#';i++){
+            if (r[1]==r[i]){
+                a2++;
+                r[i]='#';
+            }
+        }
+        for (i=3;i<5 && r[2] != '#';i++){
+            if (r[2]==r[i]){
+                a3++;
+                r[i]='#';
+            }
+        }
+        for (i=4;i<5 && r[3] != '#';i++){
+            if (r[3]==r[i]){
+                a4++;
+            }
+        }
+        int alist[4] = {a1,a2,a3,a4},max=0,t;
+        for (i=0;i<4;i++){
+            if (alist[i]>max){
+                max=alist[i];
+                t=i;
+            }
+        }
+        mytype += r[t];
     }
+    
 };
-
 
 int main() {
     mbti mbti;
@@ -178,15 +189,16 @@ int main() {
 
     cout << "Mark size (3 or 5): ";
     cin >> mark ;
+    while (mark != 3 && mark != 5) {
+        cout << "Mark size (3 or 5): ";
+        cin >> mark;
+    }
     if (mark == 3){
         mbti.mark3();
-        mbti.typembti3();
     }
     else if (mark == 5){
         mbti.mark5();
-        mbti.typembti5(mark);
     }
-
-
+    cout << name_new << " Type : " << mbti.mytype ;
     return 0;
 }
